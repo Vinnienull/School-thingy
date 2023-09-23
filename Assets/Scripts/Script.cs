5db04f69d0a84662d0d3897d39e518e5a0b0eb34
Vinnienull <59155312+Vinnienull@users.noreply.github.com>
2023-09-23 14:47:57 -0400

    Project files

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("All my files are on my laptop so I'm just doing this quickly lol");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
